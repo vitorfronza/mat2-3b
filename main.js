27d8d9c10f3fe5a58e1a6c4101d98c5e1581c485
vitorfronza <104856615+vitorfronza@users.noreply.github.com>
2024-03-22 11:12:30 -0300

    .

--- main.js
+++ main.js
@@ -18,26 +18,20 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
+let tempoAtual = new Date();
+
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
-
-
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
-
-let segundos = Math.floor(tempoFinal / 1000);
-let minutos = Math.floor
-let horas = Math.floor(minutos / 60);
-let dias = Math.floor(horas / 24);
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-
-
-return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
 }
-
-
 
